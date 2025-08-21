c68f433ac695a9a77c1c8a867b41ef25deece75c
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-21 19:09:50 +0600

    add a grade file

---
+++ Grade.java
@@ -0,0 +1,16 @@
+public class Grade {
+
+    public String calculateGrade(int marks) {
+        if (marks >= 90) {
+            return "A";
+        } else if (marks >= 80) {
+            return "B";
+        } else if (marks >= 70) {
+            return "C";
+        } else if (marks >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
