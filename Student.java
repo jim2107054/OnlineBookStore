571cf30985940a6c8ab562665758e1bf421cb4a7
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-21 18:32:47 +0600

    add a new java class

---
+++ Student.java
@@ -0,0 +1,64 @@
+public class Student {
+    // Fields
+    private int id;
+    private String name;
+    private int age;
+    private String email;
+
+    // Default constructor
+    public Student() {
+    }
+
+    // Parameterized constructor
+    public Student(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // To string method for printing
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
